906dd1d5e7e303b7655a225b59834123c350f7af
Aleksandar Balalovski <abalalovski@gmail.com>
2021-01-12 16:38:41 +0100

    initial commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/aleksandarbalalovski/dev/sdks/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/aleksandarbalalovski/dev/flutter/dadcast"
+export "FLUTTER_TARGET=/Users/aleksandarbalalovski/dev/flutter/dadcast/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/aleksandarbalalovski/dev/flutter/dadcast/.dart_tool/package_config.json"
